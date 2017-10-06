26f28fdda3469df53e6cc4bc0c887869c136de90
chris-lavington <chris.lavington@gmail.com>
2017-10-06 12:12:35 +0100

    Update contact_list_controller.js

--- server/controllers/contact_list_controller.js
+++ server/controllers/contact_list_controller.js
@@ -178,7 +178,7 @@ exports.addUser = function(req, res, next) {
 		sg.API(requestCode, function(error, response) {
 			if (error) {
 				console.log('sendOfferCode Error response received');
-				console.log(error + 'sendOfferCode Error response received');
+				console.log('error: ' + error);
 			}
 		});
 
