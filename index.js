7f2c47084b73aaaadec6b18d56504f55fbf3ec90
chris-lavington <chris.lavington@gmail.com>
2017-10-06 12:48:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const app = express();
 const router = require('./server/router');
 
 // App setup
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }, stream: process.stderr
+}));
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode >= 400
+    }, stream: process.stdout
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 router(app);
