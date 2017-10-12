8990fe85b70c825c5cd3c4d5f78aef24bd0c8683
chris-lavington <chris.lavington@gmail.com>
2017-10-12 08:48:14 +0100

    Update router.js

--- server/router.js
+++ server/router.js
@@ -5,9 +5,5 @@ module.exports = function(app) {
 	app.get('/', function(req, res) { res.sendFile(path.join(__dirname, '/static/index.html')) });
 	app.get('/success', function(req, res) { res.sendFile(path.join(__dirname, '/static/success.html')) });
 	app.post('/confirmEmail', ContactList.sendConfirmation);
-	app.post('/signup', function(req, res, next){
-		var user_email = req.body.email;
-		console.log('user_email: ' +user_email);
-		ContactList.addUser(user_email, req, res);
-	});
+	app.post('/signup', ContactList.addUser;);
 }
