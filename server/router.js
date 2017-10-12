8bc3b1269a3f7587280ac850ef96cbaf26c26bc3
chris-lavington <chris.lavington@gmail.com>
2017-10-12 08:17:25 +0100

    Update router.js

--- server/router.js
+++ server/router.js
@@ -4,11 +4,6 @@ const ContactList = require('./controllers/contact_list_controller');
 module.exports = function(app) {
 	app.get('/', function(req, res) { res.sendFile(path.join(__dirname, '/static/index.html')) });
 	app.get('/success', function(req, res) { res.sendFile(path.join(__dirname, '/static/success.html')) });
-	app.post('/confirmEmail', function(req, res){
-		var user_email = req.body.email;
-		console.log('user EMAIL: ' +user_email);
-		ContactList.sendConfirmation;
-		//ContactList.addUser(user_email, req, res);
-	});
+	app.post('/confirmEmail', ContactList.sendConfirmation);
 	app.post('/signup', ContactList.addUser);
 }
